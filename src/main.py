eafb4290ae425365f14847cb993f769fedbde62f
c0pson <pc312199@student.polsl.pl>
2024-07-07 21:39:34 +0200

    main

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@ from tile import Tile
 from board import Board
 
 class MainWindow(ctk.CTk):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         board = Board(self)
         board.pack(expand=True, padx=10, pady=10)
-        self.bind('<Up>', lambda e: board.move_up(e))
-        self.bind('<Down>', lambda e: board.move_down(e))
-        self.bind('<Right>', lambda e: board.move_right(e))
-        self.bind('<Left>', lambda e: board.move_left(e))
+        self.bind('<Up>', lambda e: board.move_up(e, 1))
+        self.bind('<Down>', lambda e: board.move_down(e, 1))
+        self.bind('<Right>', lambda e: board.move_right(e, 1))
+        self.bind('<Left>', lambda e: board.move_left(e, 1))
 
 if __name__ == "__main__":
     window = MainWindow()

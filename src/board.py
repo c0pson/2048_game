5e336d8cde758a1baf5032ada3de3b6802cc5aaa
c0pson <pc312199@student.polsl.pl>
2024-07-08 00:10:02 +0200

    win loose check

--- src/board.py
+++ src/board.py
@@ -7,6 +7,7 @@ from tile import Tile
 
 class Board(ctk.CTkFrame):
     def __init__(self, master) -> None:
+        self.main_menu = master
         super().__init__(master)
         self.board = self.create_board()
         self.display_board()
@@ -25,6 +26,7 @@ class Board(ctk.CTkFrame):
             y = random.randrange(0, 4)
         self.board[x][y].generate_number()
         self.update_board()
+        self.check_lose()
 
     def check_space(self) -> bool:
         for row in self.board:
@@ -88,6 +90,9 @@ class Board(ctk.CTkFrame):
             self.move_up(event, iter-1)
             if not self.check_if_can_move(original_board):
                 self.new_tile()
+            if self.check_lose():
+                self.restart()
+                return
         self.update_board()
 
     def sum_down(self) -> None:
@@ -114,6 +119,9 @@ class Board(ctk.CTkFrame):
             self.move_down(event, iter-1)
             if not self.check_if_can_move(original_board):
                 self.new_tile()
+            if self.check_lose():
+                self.restart()
+                return
         self.update_board()
 
     def sum_right(self):
@@ -140,6 +148,9 @@ class Board(ctk.CTkFrame):
             self.move_right(event, iter-1)
             if not self.check_if_can_move(original_board):
                 self.new_tile()
+            if self.check_lose():
+                self.restart()
+                return
         self.update_board()
 
 
@@ -167,4 +178,51 @@ class Board(ctk.CTkFrame):
             self.move_left(event, iter-1)
             if not self.check_if_can_move(original_board):
                 self.new_tile()
+            if self.check_lose():
+                self.restart()
+                return
         self.update_board()
+
+    def check_win(self):
+        for i, row in enumerate(self.board):
+                for j in range(len(row)):
+                    if self.board[i][j].number == 2048:
+                        print('u won')
+
+    def check_lose(self) -> bool:
+        original_board = [[self.board[j][i].number for i in range(4)] for j in range(4)]
+        rows = len(original_board)
+        cols = len(original_board[0])
+        for i in range(rows):
+            for j in range(cols):
+                current_number = original_board[i][j]
+                if current_number == 0:
+                    return False
+                if j > 0 and original_board[i][j - 1] == current_number:
+                    return False
+                if j < cols - 1 and original_board[i][j + 1] == current_number:
+                    return False
+                if i > 0 and original_board[i - 1][j] == current_number:
+                    return False
+                if i < rows - 1 and original_board[i + 1][j] == current_number:
+                    return False
+        return True
+
+    def restart(self):
+        self.main_menu.end_game(True)
+        self.end_frame = ctk.CTkFrame(self)
+        self.end_frame.place(relx=0.5, rely=0.5, anchor=ctk.CENTER, relwidth=1)
+        self.notification = ctk.CTkLabel(self.end_frame, font=ctk.CTkFont('Franklin Gothic', 94),
+                                        text='GAME OVER')
+        self.notification.pack(padx=10, pady=10)
+        self.restart_button = ctk.CTkButton(self.end_frame, font=ctk.CTkFont('Franklin Gothic', 94),
+                                        text='Restart', command=lambda: self.new_game(),
+                                        fg_color=COLOR.GRAY)
+        self.restart_button.pack()
+
+    def new_game(self):
+        self.board = self.create_board()
+        self.new_tile()
+        self.end_frame.destroy()
+        self.main_menu.unlock()
+        self.main_menu.start_game(True)

e1f982b192014bd9497d9c3d464414e87dff0e8f
c0pson <pc312199@student.polsl.pl>
2024-07-08 11:32:07 +0200

    score label | restart | pause

--- src/menu.py
+++ src/menu.py
@@ -1,9 +1,22 @@
 import customtkinter as ctk
+import sys
+import os
+
+from properties import COLOR
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS2
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 class Menu(ctk.CTkFrame):
     def __init__(self, master, board):
         self.main_window = master
-        super().__init__(master)
+        super().__init__(master, fg_color=COLOR.FOREGROUND)
+        ctk.FontManager.windows_load_font(resource_path('fonts\\NotoEmoji-VariableFont_wght.ttf'))
+        ctk.FontManager.windows_load_font(resource_path('fonts\\Poppins-Black.ttf'))
         self.board = board
         self.time_m = 0
         self.time_s = 0
@@ -13,10 +26,11 @@ class Menu(ctk.CTkFrame):
         self.paused = False
         self.timer()
         self.pause_button()
+        self.restart_button()
         self.main_window.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def timer(self) -> None:
-        self.time_label = ctk.CTkLabel(self, font=ctk.CTkFont('Franklin Gothic', 64),
+        self.time_label = ctk.CTkLabel(self, font=ctk.CTkFont('Poppins', 64), text_color=COLOR.TEXT_2, 
                                         text=f'{str(self.time_m).zfill(2)}:{str(self.time_s).zfill(2)}')
         self.time_label.pack(side=ctk.LEFT, padx=10, pady=10)
         self.next = self.master.after(1000, self.update_timer)
@@ -40,11 +54,14 @@ class Menu(ctk.CTkFrame):
             self.paused = False
             self.restart_timer()
             self.update_timer()
+            self.paused_label.destroy()
         elif not self.paused:
             self.paused = True
+            self.paused_frame()
         else:
             self.paused = False
             self.update_timer()
+            self.paused_label.destroy()
 
     def pause_clicked(self, event) -> None:
         if not self.paused:
@@ -53,17 +70,49 @@ class Menu(ctk.CTkFrame):
             self.main_window.start_game(False)
 
     def pause_button(self):
-        self.pause_label = ctk.CTkLabel(self, font=ctk.CTkFont('Franklin Gothic', 64),
-                                        text='\u23F8')
+        self.pause_label = ctk.CTkLabel(self, text='⏸️', font=ctk.CTkFont('Noto Emoji', 64),
+                                        text_color=COLOR.ACCENT)
         self.pause_label.pack(side=ctk.RIGHT, padx=10, pady=10)
         self.pause_label.bind('<Button-1>', lambda e: self.pause_clicked(e))
+
+    def paused_frame(self):
+        self.paused_label = ctk.CTkLabel(self.master, text='PAUSED',
+                                        font=ctk.CTkFont('Poppins', 64),
+                                        text_color=COLOR.ACCENT)
+        self.paused_label.place(relx=0.5, rely=0.5, anchor=ctk.CENTER, relwidth=1)
 
     def restart_timer(self):
         self.time_s = 0
         self.time_m = 0
         self.points = 0
 
+    def restart_button(self):
+        self.restart_label = ctk.CTkLabel(self, text='🔃', font=ctk.CTkFont('Noto Emoji', 64),
+                                            text_color=COLOR.ACCENT)
+        self.restart_label.pack(side=ctk.RIGHT, padx=10, pady=10)
+        self.restart_label.bind('<Button-1>', lambda e: self.restart(e))
+
+    def restart(self, event):
+        self.board.new_game()
+
     def on_closing(self):
         if self.next:
             self.master.after_cancel(self.next)
         self.main_window.destroy()
+
+class ScoreTable(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master, fg_color=COLOR.FOREGROUND)
+        self.points = 0
+        self.score_table()
+
+    def score_table(self):
+        self.score_label = ctk.CTkLabel(self, text=f'Score: {self.points}', font=ctk.CTkFont('Poppins', 64),
+                                            text_color=COLOR.ACCENT)
+        self.score_label.pack(side=ctk.LEFT, padx=10, pady=10)
+
+    def update_score(self):
+        self.score_label.configure(text=f'Score: {self.points}')
+
+    def restart_score(self):
+        self.points = 0

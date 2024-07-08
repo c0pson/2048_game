7faf6eb99e882143d4b9fd23ff7c2021fc8aaee0
c0pson <pc312199@student.polsl.pl>
2024-07-08 11:31:53 +0200

    main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 import customtkinter as ctk
 
+from menu import Menu, ScoreTable
 from board import Board
-from menu import Menu
+
+from properties import COLOR
 
 class MainWindow(ctk.CTk):
     def __init__(self) -> None:
-        super().__init__()
-        self.board = Board(self)
+        super().__init__(fg_color=COLOR.BACKGROUND)
         self.binds = ['<Up>', '<Down>', '<Right>', '<Left>']
+        self.score_table = ScoreTable(self)
+        self.board = Board(self)
         self.menu = Menu(self, self.board)
         self.menu.pack(padx=10, pady=10, fill='x')
         self.board.pack(expand=True, padx=10, pady=10)
+        self.score_table.pack(padx=10, pady=10, fill='x')
         self.bind('<Up>', lambda e: self.board.move_up(e, 1))
         self.bind('<Down>', lambda e: self.board.move_down(e, 1))
         self.bind('<Right>', lambda e: self.board.move_right(e, 1))
@@ -33,8 +37,13 @@ class MainWindow(ctk.CTk):
     def unlock(self):
         self.menu.game_over = False
 
+    def update_score(self, point: int):
+        self.score_table.points += point
+        self.score_table.update_score()
+
 if __name__ == "__main__":
     window = MainWindow()
-    window.geometry('720x840')
+    window.geometry('720x940')
+    window.minsize(720, 940)
     window.title('2048')
     window.mainloop()
